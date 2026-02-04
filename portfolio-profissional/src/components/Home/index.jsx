b6e0852b1f279e1986d65874852c37621a532286
Marcella <marcella.oliveira.diasss@gmail.com>
2026-02-03 21:22:19 -0300

    Altera a hierarquia de divs da section Home. Transforma fundo transparente e adiciona texto

--- portfolio-profissional/src/components/Home/index.jsx
+++ portfolio-profissional/src/components/Home/index.jsx
@@ -2,9 +2,12 @@ import "./home.style.css";
 export const Home = () => {
     return (
         <div className="container-section-home">
-            <div className="home-content">
-                <p>Inovação guiada pela experiência do usuário</p>
-                <p>Meu objetivo é.....</p>
+            <div className="home-transbox">
+                <div className="home-content">
+                    <h1>Onde inovação, design e tecnologia se encontram.</h1>
+                    <p>Crio experiências digitais modernas e funcionais, unindo design estratégico e desenvolvimento front-end.<br/>
+                    Meu foco é transformar ideias em interfaces intuitivas, responsivas e alinhadas aos objetivos de cada projeto.</p>
+                </div>
             </div>
         </div>
     )
