e7cc966b1759c953ff7e2e4d496f143a8ed27719
Marcella <marcella.oliveira.diasss@gmail.com>
2026-02-04 21:22:50 -0300

    Adicionado foto e cores de fundo da section home

--- portfolio-profissional/src/components/About/index.jsx
+++ portfolio-profissional/src/components/About/index.jsx
@@ -2,18 +2,20 @@ import "./about.styles.css"
 export const About = () => {
     return (
         <div className="container-section-sobre">
-            <div className="photo-sobre">
-                <img src="" alt="" />
+            <div className="container-content-sobre">
+                <div className="photo-sobre">
+                    <img src="./images/image-profile.jpeg" alt="" />
+                </div>
+                <div className="section-content">
+                    <p>Sobre mim</p>
+                    <div className="social-media">
+                        <ul className="social-media-list">
+                            <li>GitHub</li>
+                            <li>Linkedin</li>
+                            <li>E-mail</li>
+                        </ul>
+                    </div>
             </div>
-            <div className="section-content">
-                <p>Sobre mim</p>
-            </div>
-            <div className="social-media">
-                <ul className="social-media-list">
-                    <li>GitHub</li>
-                    <li>Linkedin</li>
-                    <li>E-mail</li>
-                </ul>
             </div>
         </div>
     )
